4319e156d1d78eff5cc1013c98338bef580be15f
HVDesigner <uctonic123@gmail.com>
2020-03-13 00:16:39 +0700

    first commit

---
+++ build/precache-manifest.1fd89f489a7c503a9c33966b8e0bc859.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db7ec0726e51cedfc6910ad584df9d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f732dbe0878b44438cd7",
+    "url": "/static/css/2.85b6ec3b.chunk.css"
+  },
+  {
+    "revision": "fc7b5607ef2ef0741efe",
+    "url": "/static/css/main.19f5cc24.chunk.css"
+  },
+  {
+    "revision": "f732dbe0878b44438cd7",
+    "url": "/static/js/2.282d426f.chunk.js"
+  },
+  {
+    "revision": "3f058c3016bf671348748c407a2eaccc",
+    "url": "/static/js/2.282d426f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7b5607ef2ef0741efe",
+    "url": "/static/js/main.8d81e59f.chunk.js"
+  },
+  {
+    "revision": "917594cbc0ea6140ddba",
+    "url": "/static/js/runtime-main.220c3c61.js"
+  }
+]);
